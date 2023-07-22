80da52fc58acda12cb35916a81b5274f0c374208
Abookar <133707756+Abookar@users.noreply.github.com>
2023-07-22 21:54:09 +0100

    Update handle_print.c

--- handle_print.c
+++ handle_print.c
@@ -8,8 +8,9 @@
  * @flags: Calculates active flags
  * @width: get width.
  * @precision: Precision specification
- * @size: Size specifier
+ * @size: Size Specifier
  * Return: 1 or 2;
+ *
  */
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	int flags, int width, int precision, int size)
